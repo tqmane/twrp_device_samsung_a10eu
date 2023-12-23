f6bc67d487b603d4c9c31475030c0f207bd2acdc
tqmane <149690876+tqmane@users.noreply.github.com>
2023-12-23 17:39:35 +0900

    Delete unneeded files

--- vendorsetup.sh
+++
@@ -1,9 +0,0 @@
-#
-# Copyright (C) 2023 The Android Open Source Project
-#
-# SPDX-License-Identifier: Apache-2.0
-#
-
-add_lunch_combo twrp_a10eu-user
-add_lunch_combo twrp_a10eu-userdebug
-add_lunch_combo twrp_a10eu-eng
